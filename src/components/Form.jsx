351c2ff62eafbf275a4a7c0f15c3fcae502eb9fc
Kyungin Na <KyunginNa@1.local>
2020-11-16 18:48:26 +0100

    To make state to store the input data, initiated state in App component, added a function to set the input values to the state, and made App component to pass down the values and the funtion to Form component.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,10 +4,24 @@ const Form = props => {
     return (
         <form>
             <label htmlFor="weight">Weight</label>
-            <input type="number" required placeholder="Weight in kgs" name="weight" id="weight" />
+            <input
+                type="number"
+                required
+                placeholder="Weight in kgs"
+                value={props.weight}
+                name="weight"
+                id="weight"
+                onChange={props.onChangeHandler}/>
             <label htmlFor="height">Height</label>
-            <input type="number" required plaveholder="Height in cm" name="height" id="height" />
-            <button>Calculate BMI</button>
+            <input
+                type="number"
+                required
+                placeholder="Height in cm"
+                value={props.height}
+                name="height"
+                id="height"
+                onChange={ props.onChangeHandler}/>
+            <button id="calculate">Calculate BMI</button>
         </form>
     )
 }
