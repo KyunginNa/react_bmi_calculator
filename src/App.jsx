01df194c784277805fde6f2e5a0d085d2297b4ca
Kyungin Na <KyunginNa@1.local>
2020-11-16 17:28:38 +0100

    Created Form component and created an acceptance test and a component test for it.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
+import Form from './components/Form'
 
 class App extends Component {
   render() {
-    return <div></div>;
+    return <Form />
   }
 }
 
